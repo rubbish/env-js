f0f9aeebf346dc9735edf546cdc5a5075e19b430
Luke Amdor <luke@absoluterubbish.net>
2009-05-12 14:53:26 -0500

    Added failing tests to expose prototype bug with Element.addMethod not adding methods to HTMLElement.prototype

--- test/unit/prototypecompat.js
+++ test/unit/prototypecompat.js
@@ -8,6 +8,20 @@ test("$ method", function(){
   equals($("header"), document.getElementById("header"));
 });
 
+test("Element.hide method", function() {
+  document.getElementById("header").style.display = "block";
+  expect(1);
+  try {Element.hide("header")}catch(e) {print(e);}
+  ok(document.getElementById("header").style.display == "none", "expected Element.hide('header') to hide the element");
+});
+
+test("Element.methods extension", function() {
+  document.getElementById("header").style.display = "block";
+  expect(1);
+  try {$("header").hide()}catch(e) {print(e);}
+  ok(document.getElementById("header").style.display == "none", "expected $('header').hide() to hide the element");
+});
+
 test("$$ method", function(){
   expect(1);
   ok($$(".chain").length == 10, "$$ found correct number of elements")
